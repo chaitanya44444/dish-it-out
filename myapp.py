0a1f3ab15756157058cd9dcffa4515424ebb42b7
Chaitanya Vaswani <vaswanichaitanya@gmail.com>
2024-10-17 19:53:52 +0530

    fixed git error
    a

--- myapp.py
+++ myapp.py
@@ -28,6 +28,27 @@ def mpp():
 
     return a
 
+@app.route('/momos')
+def mpp1():
+    img_dir = "./static/dishes/desert"
+    img_list = os.listdir(img_dir)
+    img_path = os.path.join(img_dir, random.choice(img_list))
+
+    return a
+@app.route('/spicy')
+def mpp2():
+    img_dir = "./static/dishes/spicy"
+    img_list = os.listdir(img_dir)
+    img_path = os.path.join(img_dir, random.choice(img_list))
+
+    return a
+@app.route('/sweet')
+def mpp3():
+    img_dir = "./static/dishes/sweet"
+    img_list = os.listdir(img_dir)
+    img_path = os.path.join(img_dir, random.choice(img_list))
+
+    return a
 
 
 
